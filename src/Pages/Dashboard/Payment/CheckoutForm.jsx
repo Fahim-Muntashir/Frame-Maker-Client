dd36134e9d90a662aab44f7fe469651a28e369e4
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-09-04 21:07:46 +0600

    add Public

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -6,32 +6,45 @@ const CheckoutForm = () => {
     const elements = useElements();
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if(!stripe || !elements){
+        if (!stripe || !elements) {
             return
         }
-    }
+        const card = elements.getElement(CardElement);
+        if (card === null) {
+            return
+        }
 
+        const { error,paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+        if (error) {
+            console.log('erroor',error)
+        } else {
+            console.log("payment_method",paymentMethod)
+        }
+    }
     return (
-        <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
-              },
-            },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
+        <form className="w-2/3 m-8" onSubmit={handleSubmit}>
+        <CardElement
+            options={{
+                style: {
+                    base: {
+                        fontSize: '16px',
+                        color: '#424770',
+                        '::placeholder': {
+                            color: '#aab7c4',
+                        },
+                    },
+                    invalid: {
+                        color: '#9e2146',
+                    },
+                },
+            }}
+        />
+        <button className="btn btn-primary btn-sm" type="submit" disabled={!stripe}>
+            Pay
+        </button>
     </form>
     );
 };
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import CheckoutForm from './CheckoutForm';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise =loadStripe(process.env.RREACT_APP_Payment_Gateway_PK);
+
 
 const Payment = () => {
     return (
-        <div className='text-center'>
+        <div className='text-center my-auto mx-auto'>
             <h1>Provide your Payment Info</h1>
+            <Elements stripe={stripePromise}>
+                  <CheckoutForm></CheckoutForm>
+           </Elements>
+          
         </div>
     );
 };
